6d482ca1aba3ec3908048311a1d8a96bcb0c9439
Shinigamiiz <119001740+Shinigamiiz@users.noreply.github.com>
2022-12-11 01:40:51 +0530

    Update start.py

--- Scanner/plugins/start.py
+++ Scanner/plugins/start.py
@@ -19,7 +19,7 @@ sree = TelegramClient(
 
 Owner = "iveL83"
 repo = "https://github.com"
-@sree.on(events.NewMessage(pattern="^/start"))
+@sree.on(events.NewMessage(pattern="start"))
 async def start(event):
     buttns = [Button.url("••ѕυρροяτ••", "https://t.me/NezukoKamado"), Button.url("••ʀєρο••", f'{repo}')]
     py = platform.python_version()
