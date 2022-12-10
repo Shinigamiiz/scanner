415257a84b8180d7275e01a094e7edc3a0efccda
Shinigamiiz <119001740+Shinigamiiz@users.noreply.github.com>
2022-12-10 23:19:59 +0530

    Update pmmsg.py

--- Scanner/plugins/pmmsg.py
+++ Scanner/plugins/pmmsg.py
@@ -10,5 +10,5 @@ from Scanner import ubot
 
 @ubot.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: ubot, message: Message):
-  await ubot.send_message(message.chat.id,f"Hey 👋 I am the assistant of Shikimori Music bot, didn't have a time to talk with you 🙂 kindly join @{SUPPORT_CHAT} for getting Support.")
+  await ubot.send_message(message.chat.id,f"Dɪᴅɴ'ᴛ ʜᴀᴠᴇ ᴀ ᴛɪᴍᴇ ᴛᴏ ᴛᴀʟᴋ ᴡɪᴛʜ ʏᴏᴜ 🙂 ᴋɪɴᴅʟʏ ᴊᴏɪɴ @{SUPPORT_CHAT} ғᴏʀ ɢᴇᴛᴛɪɴɢ Sᴜᴘᴘᴏʀᴛ.")
   return
