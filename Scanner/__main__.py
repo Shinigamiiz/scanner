a7d3db44451667f9e43dc051efd9950bac23974c
Levi <119001740+Shinigamiiz@users.noreply.github.com>
2022-12-20 08:57:51 +0530

    Update __main__.py

--- Scanner/__main__.py
+++ Scanner/__main__.py
@@ -1,51 +1,44 @@
 import asyncio
-import os
 import requests
 
 from pyrogram import Client
-from pyrogram.enums import ParseMode
+from pytgcalls import idle
 
 from Scanner import LOGGER, pbot, ubot, tbot
 from Scanner.db.global_bans_db import num_gbanned_users
 from Scanner.vars import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL_ID
 
-DOWNLOAD_DIRECTORY = "/tmp"
-
 async def load_start():
     count = num_gbanned_users()
-    LOGGER.info(f"Current Gbanned Users: {count}")
     LOGGER.info("[INFO]: STARTED")
-    LOGGER.info(f"LOG CHANNELS: {int(LOG_CHANNEL_ID)}")
     try:
         await pbot.send_message(
-            int(LOG_CHANNEL_ID), f"**Pyrogram Client Started Successfully !!**\nCurrent Gbanned Users: `{count}`"
+            LOG_CHANNEL_ID, f"**Pyrogram Client Started Successfully !!**\nCurrent Gbanned Users: `{count}`"
         )
         LOGGER.info("[INFO]: PYROGRAM BOT STARTED")
-    except Exception as e:
-        LOGGER.info(f"Bot wasn't able to semd message in your log channel.\n\nERROR: {e}")
+    except:
+        LOGGER.info("Bot wasn't able to semd message in your log channel.")
     try:
         await ubot.send_message(
-            int(LOG_CHANNEL_ID), "**Assistant Started Successfully !!**"
+            LOG_CHANNEL_ID, "**Assistant Started Successfully !!**"
         )
         LOGGER.info("[INFO]: PYROGRAM UserBOT STARTED")
-    except Exception as e:
-        LOGGER.info(f"UserBot wasn't able to semd message in your log channel.\n\nERROR: {e}")
+    except:
+        LOGGER.info("UserBot wasn't able to semd message in your log channel.")
     
 
 loop = asyncio.get_event_loop_policy().get_event_loop()
 loop.run_until_complete(load_start())
 
+tbot.start(bot_token=BOT_TOKEN)
+
 Client(
-    name="SOME-1HING",
-    api_id=API_ID,
-    api_hash=API_HASH,
+    ":memory:",
+    API_ID,
+    API_HASH,
     bot_token=BOT_TOKEN,
-    workers=min(32, os.cpu_count() + 4),
-    parse_mode=ParseMode.DEFAULT,
-    workdir=DOWNLOAD_DIRECTORY,
-    sleep_threshold=60,
-    in_memory=True,
     plugins={"root": "Scanner.plugins"},
 ).start()
 
+idle()
 loop.close()
