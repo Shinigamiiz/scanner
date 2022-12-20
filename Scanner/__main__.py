9841d8667485374636c6f16b76245c7b90512f46
Levi <119001740+Shinigamiiz@users.noreply.github.com>
2022-12-20 07:46:14 +0530

    Update __main__.py

--- Scanner/__main__.py
+++ Scanner/__main__.py
@@ -32,7 +32,7 @@ async def load_start():
         LOGGER.info(f"UserBot wasn't able to semd message in your log channel.\n\nERROR: {e}")
     
 
-loop = asyncio.get_event_loop()
+loop = asyncio.new_event_loop()
 asyncio.get_event_loop()
 
 Client(
